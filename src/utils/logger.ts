7b9f9cb68f377eeb0844c13fe7c7f39e2249ad56
Rahul Mondal <rahulmondar@gmail.com>
2023-02-25 12:24:52 +0530

    fix: socket.ts file

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,15 +5,15 @@ import config from "config";
 
 // logger initialization
 const logger = pino({
-  transport: {
-    target: "pino-pretty",
-    options: { colorize: true },
-  },
-  base: {
-    pid: false,
-  },
-  level: config.get<string>("log_level"),
-  timestamp: () => `,"time":"${dayjs().format()}"`,
+    transport: {
+        target: "pino-pretty",
+        options: { colorize: true },
+    },
+    base: {
+        pid: false,
+    },
+    level: config.get<string>("log_level"),
+    timestamp: () => `,"time":"${dayjs().format()}"`,
 });
 
 export default logger;
